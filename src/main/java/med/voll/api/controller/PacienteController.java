5ca113446eb80ab87b57b27346033b4a4ae3e2de
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-04-15 08:30:59 -0300

    creating entity paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,19 +1,28 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
 	
+	@Autowired
+	PacienteRepository repository;
+	
 	@PostMapping
-	public void cadastar (@RequestBody DadosCadastroPaciente dados) {
-		System.out.println("dados recebidos" + dados);
+	@Transactional
+	public void cadastar (@RequestBody @Valid DadosCadastroPaciente dados) {
+		repository.save(new Paciente(dados));
 	}
 
 }
